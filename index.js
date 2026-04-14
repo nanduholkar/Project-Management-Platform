8e32fa436df82ee71a395ee181c39803d2f896df
nanduholkar <nanduholkar7@gmail.com>
2026-04-15 00:00:54 +0530

    chore: configure dotenv and update project formatting

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Start of backend Project");
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env"
+})
+
+let myusername = process.env.user_name
+
+console.log("value", myusername);
+
+
+console.log("Start of the backend Project");
