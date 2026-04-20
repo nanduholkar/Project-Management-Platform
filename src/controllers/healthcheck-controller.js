71b2412c7422fedde878041ba32bef251f9a7aeb
nanduholkar <nanduholkar7@gmail.com>
2026-04-20 18:00:19 +0530

    Refactor healthCheck controller to use asyncHandler and standardized ApiResponse

--- src/controllers/healthcheck-controller.js
+++ src/controllers/healthcheck-controller.js
@@ -1,17 +1,26 @@
 import {ApiResponse} from "../utils/api-response.js"
+import { asyncHandler } from "../utils/async-handler.js"
 
-const healthCheck = (req, res) =>{
-    try{
-        res
-        .status(200)
-        .json(
-            new ApiResponse(200, {message: "Server is running"})
+
+// const healthCheck = async (req, res) =>{
+//     try{
+//         const user = await getUserFromDB()
+//         res
+//         .status(200)
+//         .json(
+//             new ApiResponse(200, {message: "Server is running"})
         
-        )
+//         )
 
-    }catch(error){
-        res.status(500).json(new ApiResponse(500,null,"something went wrong"))
+//     }catch(error){
+//         next(err)
 
-    };
-}
+//     };
+// }
+
+const healthCheck = asyncHandler(async (req, res) => {
+    res
+    .status(200)
+    .json(new ApiResponse(200,{message:"server is running"}))
+})
 export {healthCheck}
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+app.use("/api/v1/healthcheck", healthCheckRouter)
