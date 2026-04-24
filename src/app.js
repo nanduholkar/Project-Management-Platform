201869a1e1cf8fe8a24927bd475753c57c81296c
nanduholkar <nanduholkar7@gmail.com>
2026-04-25 02:10:30 +0530

    chore: add cookie-parser middleware for handling HTTP cookies

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
-
+import cookieParser from "cookie-parser"
 
 const app = express()
 // Basic Configuration 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
 
 // cors configurations
 app.use(cors({
