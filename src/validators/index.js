d3f16f4082997c2ca74832287eb8de880354774e
nanduholkar <nanduholkar7@gmail.com>
2026-04-25 02:07:53 +0530

    feat: implement request validation layer for authentication endpoints using express-validator

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body} from "express-validator";
 
 const userRegisterValidator = () => {
     return [
@@ -7,4 +7,61 @@ const userRegisterValidator = () => {
         body("password").trim().notEmpty().withMessage("Password is required "),
         body("fullName").optional().trim()
     ]
+}
+
+
+
+const userLoginValidator = () =>{
+    return[
+        body('email')
+          .optional()
+          .isEmail()
+          .withMessage("Email is Invalid"),
+        body("username")
+          .optional()
+          .isLength({ min : 3})
+          .withMessage("username must be at least 3 chars"),
+        body("password")
+          .notEmpty()
+          .withMessage("Password is requied")
+          
+    ]
+}
+
+const userChangeCurrentPasswordValidator = () => {
+  return[
+    body("oldPassword").notEmpty().withMessage("old password is required"),
+    body("newPassword").notEmpty().withMessage("New password is required")
+  ]
+}
+
+const userForgetPasswordValidator = () =>{
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage("Email is required")
+      .isEmail()
+      .withMessage("Email is Invalid")
+  ]
+}
+
+const userResetForgetPasswordValidator = () =>{
+  return [
+    body("newPassword")
+      .notEmpty()
+      .withMessage("Password is required")
+    
+
+  ]
+}
+
+export { 
+  userRegisterValidator, 
+  userLoginValidator,
+  userChangeCurrentPasswordValidator,
+  userResetForgetPasswordValidator,
+  userForgetPasswordValidator,
+
+
+
 }
